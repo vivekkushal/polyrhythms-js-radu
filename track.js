efbb8c9feee032983b0c999579b35ddf3354ddac
vivekkushal <vivekjss1@gmail.com>
2024-12-20 23:24:32 +0530

    fead: Add ball object

--- track.js
+++ track.js
@@ -4,6 +4,13 @@ class Track {
     this.radius = radius;
   }
 
+  getPosition(offset) {
+    return {
+      x: this.center.x + Math.cos(offset) * this.radius,
+      y: this.center.y - Math.sin(offset) * this.radius,
+    };
+  }
+
   draw(ctx) {
     ctx.beginPath();
     ctx.arc(this.center.x, this.center.y, this.radius, 0, Math.PI * 2);
